77241b9a44d3dc7d8ded135d5c7bd91259f5049b
Wasasita <102564909+Wasasita@users.noreply.github.com>
2022-10-08 14:12:17 -0400

    Update Ana_LaRosa_LiveCode.py

--- liveCodeOne/Ana_LaRosa_LiveCode.py
+++ liveCodeOne/Ana_LaRosa_LiveCode.py
@@ -4,7 +4,7 @@ from chessboard import display
 from time import sleep
 
 # display time between moves in seconds
-Time = 1
+TIME = 1
 
 # create board object
 board = chess.Board()
@@ -16,7 +16,7 @@ game_board = display.start()
 while True:
     display.check_for_quit()
     legal_moves = list(board.legal_moves)
-    enginesmove = random.choice(legal_moves)
+    enginemove = random.choice(legal_moves)
     board.san_and_push(enginemove)
     valid_fen = board.fen()
     display.update(valid_fen, game_board)
